2f6671f199910e55f32b1d0fcdf80031be7d4fe8
karinasv <karina.svanstrom@gmail.com>
2023-06-02 18:35:29 +0200

    added comments

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 "use strict";
 
+// Variables to declare a constant (unchanging) variable
+
 const cat_btn = document.getElementById("cat-btn");
 const fact_btn = document.getElementById("fact-btn");
 const fact_result = document.getElementById("fact-result");
 const cat_image = document.getElementById("cat-image");
 
+// Get random cat pictures when a user clicks a button
+
 cat_btn.addEventListener("click", async () => {
     await getRandomCat();
 });
 
+// Get random cat facts when a user clicks a button
+
 fact_btn.addEventListener("click", async () => {
     await getRandomFact();
 });
+
+// Get random cat pictures
 
 async function getRandomCat() {
     try {
@@ -21,12 +29,16 @@ async function getRandomCat() {
         if (data && data.length > 0) {
             const imageUrl = data[0].url;
             cat_image.src = imageUrl;
+
+            //  Store the cat picture URL in localStorage
             localStorage.setItem("catPicture", imageUrl);
         }
     } catch (error) {
         console.log(error);
     }
 }
+
+// Get random cat facts
 
 async function getRandomFact() {
     try {
@@ -35,6 +47,8 @@ async function getRandomFact() {
 
         if (data.data) {
             fact_result.innerHTML = `<i>${data.data}</i>`;
+
+            // Store the cat fact in localStorage
             localStorage.setItem("catFact", data.data);
         } else {
             fact_result.innerHTML = "<i>No cat fact available</i>";
